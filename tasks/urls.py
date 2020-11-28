c982df28bcb40ef3126127e8e2888ee5755e4c9b
VadimKukuzia <vadimol081@gmail.com>
2020-11-28 16:06:22 +0200

    Made list of notes

--- tasks/urls.py
+++ tasks/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="index"),
-    path('update_task/<str:pk>/', views.update_task, name="update_task"),
-    path('delete_task/<str:pk>/', views.delete_task, name="delete_task")
+    path('<str:list_id>/tasks', views.list_tasks, name="tasks"),
+    path('<str:list_id>/tasks', views.list_tasks, name="add_task"),
+    path('<str:list_id>/tasks/update_task/<str:pk>/', views.update_task, name="update_task"),
+    path('<str:list_id>/tasks/delete_task/<str:pk>/', views.delete_task, name="delete_task")
 ]
