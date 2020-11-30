21db6cca1c01d0cba2a552c857981e4a7a966092
VadimKukuzia <vadimol081@gmail.com>
2020-11-30 14:38:28 +0200

    deploy

--- KursWeb/settings.py
+++ KursWeb/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '...'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'https://my-note.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'https://my-note.herokuapp.com', '0.0.0.0', 'localhost']
 
 
 # Application definition
