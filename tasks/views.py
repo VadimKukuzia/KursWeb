050d599801b94510efe15ee37a334760dff79745
VadimKukuzia <vadimol081@gmail.com>
2020-12-03 17:21:30 +0200

    Added description

--- tasks/views.py
+++ tasks/views.py
@@ -1,18 +1,12 @@
-import os
-from sys import path
 
 from django.conf.global_settings import EMAIL_HOST
 from django.contrib import messages
 from django.contrib.auth import login, logout
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.forms import UserChangeForm
 from django.core import mail
-from django.core.files import File
 from django.core.mail import send_mail
-from django.http import HttpResponse, FileResponse
+from django.http import FileResponse
 from django.shortcuts import render, redirect
-
-from KursWeb import settings
 from .forms import *
 
 
@@ -20,6 +14,11 @@ from .forms import *
 
 
 def log_out(request):
+    """Метод выхода из аккаунта.
+
+       Используется, когда пользователь нажимает на кнопку выхода, в этом случае вызывается POST запрос
+       Метод с помощью определения пользователя по запросу удаляет его сессию и прекращает сеанс.
+    """
     if request.method == 'POST':
         logout(request)
         return redirect('index')
@@ -27,6 +26,12 @@ def log_out(request):
 
 
 def sign_in(request):
+    """Метод авторизации в аккаунт.
+
+        Используется, когда пользователь нажимает на кнопку входа, в этом случае вызывается POST запрос
+        Метод с помощью определения пользователя по запросу и введенных пользователем в форме авторизации данных,
+        проверяет наличие пользователя в базе данных, в случае успеха - создаёт сессию и авторизует его.
+    """
     if request.method == 'POST':
         form = AuthenticationForm(request.POST, data=request.POST)
         if form.is_valid():
@@ -43,6 +48,12 @@ def sign_in(request):
 
 
 def sign_up(request):
+    """Метод регистрации.
+
+        Используется, когда пользователь нажимает на кнопку регистрации, в этом случае возвращает форму для
+        заполнения. Далее, после введения пользователем желаемых данных, проверяих их на валидность, и, в случае
+        корректного введения, сохраняет, создает сессию и авторизует пользователя, возвращая его на начальную страницу.
+    """
     if request.method == 'POST':
         form = UserRegistrationForm(request.POST)
         if form.is_valid():
@@ -56,6 +67,11 @@ def sign_up(request):
 
 
 def index(request):
+    """Метод возврата начальной страницы.
+
+        Возвращает стартовую страницу пользователю, в случае, если он уже авторизован, возвращает список его
+        заметок с возможностью добавить ещё
+    """
     lists = TaskList.objects.filter(user_id=request.user.id).order_by('id')
 
     if request.method == 'POST':
@@ -72,6 +88,13 @@ def index(request):
 
 
 def update_email(request):
+    """Метод обновления электронной почты.
+
+        В случае, когда пользователю необходимо редактировать данные о себе, а точнее - редактировать свою почту,
+        возвращает страницу редактирования с полем для ввода новой почты пользовалеля.
+        Когда получает новую почту, проверяет её на корректность ввода, в случае успеха - перезаписывает
+        данные пользователя и возвращает его на начальную страницу, иначе - уведомит пользователя об ошибках.
+    """
     lists = TaskList.objects.filter(user_id=request.user.id).order_by('id')
     form = UserEmailEditForm(instance=request.user)
 
@@ -82,11 +105,15 @@ def update_email(request):
             form.save()
             return redirect('index')
 
-    return render(request, 'tasks/update-email.html', {'form': form, 'lists':lists})
+    return render(request, 'tasks/update-email.html', {'form': form, 'lists': lists})
 
 
 @login_required(login_url="/sign-in")
 def delete_list(request, list_id):
+    """Метод удаления списка заметок.
+
+        По выбору пользователя удаляет список заметок со всеми заметками внутри.
+    """
     task_list = TaskList.objects.get(id=list_id)
 
     if request.method == 'POST':
@@ -98,6 +125,12 @@ def delete_list(request, list_id):
 
 @login_required(login_url="/sign-in")
 def list_tasks(request, list_id):
+    """Метод показа списка.
+
+        Возвращает страницу с заметками конкретного списка. Позволяет создавать новые заметки, редактировать и удалять
+        их. В случае, если пользователь попытается адресованно перейти к чужому списку, уведомит его об отсутствии
+        доступа к запрашиваемому списку
+    """
     tasks = Task.objects.filter(task_list_id=list_id).order_by('id')
     lists = TaskList.objects.filter(user_id=request.user.id).order_by('id')
 
@@ -122,6 +155,12 @@ def list_tasks(request, list_id):
 
 @login_required(login_url="/sign-in")
 def update_task(request, pk, list_id):
+    """Метод обновления заметки.
+
+        В случае, когда пользователю необходимо редактировать заметку,
+        возвращает страницу редактирования с полем для ввода новой информации.
+        Перезаписывает данные и возвращает пользователя на страницу списка.
+    """
     task = Task.objects.get(id=pk)
     tasks = Task.objects.all().order_by('id')
     lists = TaskList.objects.filter(user_id=request.user.id).order_by('id')
@@ -141,6 +180,10 @@ def update_task(request, pk, list_id):
 
 @login_required(login_url="/sign-in")
 def delete_task(request, pk, list_id):
+    """Метод удаления заметки.
+
+        По выбору пользователя удаляет заметку из списка.
+    """
     task = Task.objects.get(id=pk)
 
     if request.method == 'POST':
@@ -152,6 +195,12 @@ def delete_task(request, pk, list_id):
 
 @login_required(login_url="/sign-in")
 def send_list_by_email(request, list_id):
+    """Метод отправки списка заметок по почте.
+
+        Проверяет список на наличие заметок, если он не пуст - формирует письмо со списком заметок, и отправляет
+        на почту, которую пользователь указал при регистрации, либо изменил позже, после отправки показывает
+        уведомлении о результате отправки.
+    """
     task_list = TaskList.objects.get(id=list_id)
     tasks = Task.objects.filter(task_list_id=list_id).order_by('id')
     email = request.user.email
@@ -171,7 +220,8 @@ def send_list_by_email(request, list_id):
                              message=text,
                              fail_silently=False,
                              connection=connection) != 0:
-                    messages.success(request, f'Письмо со списком "{task_list}" было успешно отправлено на почту {email}')
+                    messages.success(request, f'Письмо со списком "{task_list}" '
+                                              f'было успешно отправлено на почту {email}')
                     return redirect('tasks', list_id=list_id)
                 else:
                     messages.warning(request, 'Не удалость отправить.\nПопробуйте ещё раз')
@@ -185,6 +235,12 @@ def send_list_by_email(request, list_id):
 
 @login_required(login_url="/sign-in")
 def download_file(request, list_id):
+    """Метод загрузки файла.
+
+        Проверяет список на наличие заметок, если он не пуст - формирует файл со списком заметок.
+        Возвращает файловый ответ, который пользователь может сохранить.
+        Выводит уведомление об успешности операции
+    """
     task_list = TaskList.objects.get(id=list_id)
     tasks = Task.objects.filter(task_list_id=list_id).order_by('id')
     text = ''
